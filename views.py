58e28bdfa870ada2918fc618f2b4abb4f4cfad11
Fanor1234 <luis_faurp@hotmail.com>
2023-05-08 23:10:17 -0400

    cambios

--- views.py
+++ views.py
@@ -1,30 +1,15 @@
-from flask import Blueprint, render_template, request,jsonify,redirect,url_for
-import datetime
+from flask import Blueprint, render_template
 
 views = Blueprint(__name__,"views")
 
 @views.route('/')
-def hello():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+def Inicio():
+    return render_template('index.html')
 
 @views.route('/about/')
 def about():
     return render_template('about.html')
 
-@views.route('/comments/')
-def comments():
-    comments = ['This is the first comment.',
-                'This is the second comment.',
-                'This is the third comment.',
-                'This is the fourth comment.'
-                ]
-
-    return render_template('comments.html', comments=comments)
-
-@views.route('/prueba/')
-def prueba():
-    return render_template('indexp.html')
-
-@views.route('/prueba1/')
-def prueba1():
-    return render_template('indexp1.html')
+@views.route('/item/')
+def item():
+    return render_template('item.html')
